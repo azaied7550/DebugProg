3da9fbf801e5780637fe28875e90c32804e48630
Ahmed Zaied <azaied7550@gmail.com>
2017-10-11 14:12:55 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            // Changed data type from int to double
+
+            double x, y;
+
+            x = 3.1415;
+            y = 3.64;
+
+            System.out.println("pi is approximately " + x);
+            System.out.println("My GPA was " + y);
+        }
+    }
